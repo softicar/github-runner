ca646327c3cec6f4343cb4a711a8094132d1fffe
Oliver Richers <oliver.richers@preventgroup.com>
2021-10-08 09:45:52 +0200

    renamed script, added description and fixed some requests

--- nexus.sh
+++ nexus.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+# This script facilitates starting and stopping the Sonatype Nexus Repository Manager 3.
+#
+# Besides other tasks, Sonatype Nexus Repository Manager 3 can be employed as a proxy to
+# cache Maven artifacts and Docker images. There are has many other usecases.
+#
+# This script employs the `sonatype/nexus3` Docker image. The script automatically creates
+# a dedicated Docker volume on the host to store data persistently.
+#
+# For reference: https://hub.docker.com/r/sonatype/nexus3/
+
 CONTAINER_NAME="nexus"
 IMAGE_NAME="sonatype/nexus3"
 VOLUME_NAME="nexus-data"
+
+MAVEN_PROXY_PORT=8081
+DOCKER_PROXY_PORT=8123
 
 function is_running {
 	[ "`docker ps -q -f name=$CONTAINER_NAME`" != "" ]
 }
 
 function volume_exists {
-	[ "`docker volume ls -q -f name=$VOLUME_NAME -q`" != "" ]
+	[ "`docker volume ls -q -f name=$VOLUME_NAME`" != "" ]
 }
 
 function print_status {
@@ -35,7 +48,13 @@ function create_volume {
 
 function start_nexus {
 	echo "Starting container '$CONTAINER_NAME'..."
-	docker run --rm -d -p 8081:8081 -p 8123:8123 -v $VOLUME_NAME:/nexus-data --name $CONTAINER_NAME $IMAGE_NAME
+	docker run \
+		--rm -d \
+		--name $CONTAINER_NAME \
+		-v $VOLUME_NAME:/nexus-data \
+		-p $MAVEN_PROXY_PORT:$MAVEN_PROXY_PORT \
+		-p $DOCKER_PROXY_PORT:$DOCKER_PROXY_PORT \
+		$IMAGE_NAME
 }
 
 function stop_nexus {
