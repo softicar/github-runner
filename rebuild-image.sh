37e3e8d3bac1e2b0a3edc53348245f1808a4f07a
Alexander Schmidt <alexander.schmidt@preventgroup.com>
2021-10-12 22:31:18 +0200

    Remove obsolete services and shell scripts, superseded by docker-compose based approach

--- rebuild-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# TODO integrate this into softicar-github-runner.sh, complete with auto-updates by querying https://api.github.com/repos/actions/runner/releases before each restart. should be quite simple.
-
-docker build -t softicar/softicar-github-runner .
-docker image prune -f
