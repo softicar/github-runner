28a23a047383016c32792b2f1bf569d0a9a11662
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-04-05 13:43:24 +0200

    PLAT-777-3 Auto-enable anonymous access (#44)

--- nexus-image/configure.sh
+++ nexus-image/configure.sh
@@ -29,6 +29,10 @@ configure_nexus() {
   echo "${new_password}" > $ADMIN_PASSWORD_NEW_FILE && \
   log "Admin password changed. New password was written to: $ADMIN_PASSWORD_NEW_FILE"
 
+  # Enable anonymous access
+  curl -sf -u "admin:${new_password}" -X PUT -H "Content-Type: application/json" -d '{ "enabled": true }' "${NEXUS_REST_BASE_URL}/v1/security/anonymous" -o /dev/null && \
+  log "Anonymous access enabled."
+
   # Create repo: docker-hub
   curl -sf -u "admin:${new_password}" -X POST -H "Content-Type: application/json" -d "@$NEXUS_DATA_SOFTICAR_DIR/docker-hub.repository.json" "${NEXUS_REST_BASE_URL}/v1/repositories/docker/proxy" -o /dev/null && \
   log "Added repository: docker-hub"
